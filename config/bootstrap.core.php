fd3f79d5bdff126b60f6cd511d7118396c40d0fb
jamiemill <jamiermill@gmail.com>
2010-03-31 08:28:14 +0100

    Adding default config values to the bootstrap.core.php file

--- config/bootstrap.core.php
+++ config/bootstrap.core.php
@@ -16,4 +16,37 @@ define('USER_REGISTER_ACTIVATION_ADMIN_NOTIFY','adminNotify');
 // with trailing slash
 Configure::write('MissionControl.pageViewsFolder', APP . 'views' . DS . 'pages' . DS);
 
+// These should be customised by overriding in {APP}.config/bootstrap.php
+
+Configure::write('Site.title', 'MissionControl Demo Site');
+Configure::write('Site.emailsTo','website@example.com');
+Configure::write('Site.allowPageParentContent', null); 									
+Configure::write('Site.extraThumbnailSizes', array(
+	//'thumbnail'=>array(200,200,1,'C'),
+	//'homepage'=>array(450,450,1,'C')
+));
+
+Configure::write('Robot.fromAddress', 'Example <website@example.com>');
+Configure::write('Robot.SMTP.port',null);
+Configure::write('Robot.SMTP.type',null);
+Configure::write('Robot.SMTP.host',null);
+Configure::write('Robot.SMTP.username','website@example.com');
+Configure::write('Robot.SMTP.password',null);
+Configure::write('SwiftEmail.debug',true);
+
+Configure::write('User.Register.redirect',false);
+Configure::write('User.Login.fallbackRedirect','/admin/');
+Configure::write('User.SiteHasFrontendLogin',false);
+
+
+/**
+* Pick one of these three constants to define what happens when a user registers at the site:
+* 
+* 'USER_REGISTER_ACTIVATION_IMMEDIATE'
+* 'USER_REGISTER_ACTIVATION_SELF_ACTIVATE'
+* 'USER_REGISTER_ACTIVATION_ADMIN_NOTIFY'
+*/
+
+Configure::write('User.Register.activation',USER_REGISTER_ACTIVATION_ADMIN_NOTIFY);
+
 ?>
